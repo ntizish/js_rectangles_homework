1c94d97996a3a04ba308e3c965c202284c152976
ntizish <dan.perednja@gmail.com>
2022-02-03 19:09:48 +0300

    Homework done.

--- javascripts/js.js
+++ javascripts/js.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 $(".blue").click(function(){
-$(this).toggleClass("green");
+$(this).toggleClass("green"); {
+}
 });
 });
 
@@ -21,5 +22,60 @@ $(document).ready(function(){
 				return parseFloat (value) -50;
 			}
 		});
+	});
+});
+
+$(document).ready(function(){
+	$(".black").click(function(){
+		$(this).css({
+			height: function(index, value) {
+				return parseFloat (value) -50;
+			}
+		});
+		$(this).css({
+			width: function(index, value) {
+				return parseFloat (value) -50;
+			}
+		});
+	});
+});
+
+$(document).ready(function(){
+	$(".green").click(function(){
+		$(".green").toggleClass("rounded-corners");
+	});
+});
+
+$(document).ready(function(){
+	$(".magenta").click(function(){
+		$(".magenta").toggleClass("rotated-ninty");
+	});
+});
+
+$(document).ready(function(){
+	$(".purple").click(function(){
+		$(".purple").toggleClass("skewed");
+	});
+});
+
+$(document).ready(function(){
+	$(".red").click(function(){
+		$(".red").toggleClass("ran");
+	});
+});
+
+$(document).ready(function(){
+	$(".gray").click(function(){
+		$(this).css({
+			opacity: function(index, value) {
+				return parseFloat (value) -0.1;
+			}
+		});
+	});
+});
+
+$(document).ready(function(){
+	$(".orange").click(function(){
+		$(".orange").toggleClass("bye");
 	});
 });
